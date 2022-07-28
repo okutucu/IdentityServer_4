a9b3112c2c65ab30daf18df95018bc4be1c71028
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-07-28 14:31:23 +0300

    Added client credentials, api resource and api scope

---
+++ IdentityServer.AuthServer/Config.cs
@@ -0,0 +1,37 @@
+﻿using IdentityServer4.Models;
+
+namespace IdentityServer.AuthServer
+{
+    public static class Config
+    {
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>()
+            {
+                new ApiResource("resource_api_1")
+                {
+                    Scopes={ "api1.read", "api1.write" , "api1.update" }
+                },
+                new ApiResource("resource_api_2")
+                {
+                      Scopes={ "api2.read", "api2.write" , "api2.update" }
+                },
+            };
+        }
+
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new List<ApiScope>()
+            {
+                new ApiScope("api1.read","API 1 için okuma izni"),
+                new ApiScope("api1.write","API 1 için yazma izni"),
+                new ApiScope("api1.update","API 1 için güncelleme izni"),
+                new ApiScope("api2.read","API 2 için güncelleme izni"),
+                new ApiScope("api2.write","API 2 için güncelleme izni"),
+                new ApiScope("api2.update","API 2 için güncelleme izni"),
+            
+            };
+        }
+    }
+}
